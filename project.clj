2e38cff2bf2528829ee11bd3d6ee26d8e1b21c62
Anton Podviaznikov <podviaznikov@gmail.com>
2013-06-21 22:34:51 -0600

    add license and description

--- project.clj
+++ project.clj
@@ -1,6 +1,7 @@
 (defproject mangopay "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+  :description "A Clojure library for interaction with MangoPay REST API."
+  :signing {:gpg-key "HashObject Ltd <team@hashobject.com>"}
+  :url "https://github.com/hashobject/mangopay"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.5.1"]])
