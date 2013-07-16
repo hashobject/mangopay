37a574d3c2743af216bacda181e2670285555622
Anton Podviaznikov <podviaznikov@gmail.com>
2013-07-16 10:57:02 -0600

    update metadata

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (defproject mangopay "0.1.0-SNAPSHOT"
   :description "A Clojure library for interaction with MangoPay REST API."
-  :signing {:gpg-key "HashObject Ltd <team@hashobject.com>"}
+  :signing {:gpg-key "Hashobject Ltd <team@hashobject.com>"}
   :url "https://github.com/hashobject/mangopay"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
