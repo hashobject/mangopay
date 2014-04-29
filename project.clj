298f662627f9a447518c12ee5e0f29a60a12d245
Anton Podviaznikov <podviaznikov@gmail.com>
2014-04-30 00:52:37 +0200

    bump versions

--- project.clj
+++ project.clj
@@ -4,8 +4,8 @@
   :url "https://github.com/hashobject/mangopay"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.5.1"]
+  :dependencies [[org.clojure/clojure "1.6.0"]
                  [org.clojure/data.codec "0.1.0"]
                  [rsa-signer "0.1.0"]
-                 [clj-http "0.7.8"]
+                 [clj-http "0.9.1"]
                  [cheshire "5.3.1"]])
